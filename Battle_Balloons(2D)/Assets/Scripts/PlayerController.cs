deca62694ccdd93fe7954507a960e93389aa8634
Patrick <patrick.braud@ttu.edu>
2015-09-15 21:48:17 -0500

    Base Level
    Play area and moveable character

---
+++ Battle_Balloons(2D)/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody2D rb;
+
+	public float speed;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		
+		rb.AddForce (movement * speed);
+	}
+}
