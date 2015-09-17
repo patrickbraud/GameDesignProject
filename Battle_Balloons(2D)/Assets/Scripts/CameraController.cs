9376c91bcaec2145597e10228d5687f8fcacb7d9
Patrick Braud <patrick.braud@ttu.edu>
2015-09-16 19:57:54 -0500

    Added jump cooldown
    still a problem with limiting the camera on different resolutions. Needs
    to be fixed

--- Battle_Balloons(2D)/Assets/Scripts/CameraController.cs
+++ Battle_Balloons(2D)/Assets/Scripts/CameraController.cs
@@ -4,16 +4,37 @@ using System.Collections;
 public class CameraController : MonoBehaviour {
 
 	public GameObject player;
+	public float xMin, xMax, yMin, yMax;
 	
 	private Vector3 offset;
+
+	public float mapX;
+	public float mapY;
+
+
 	
 	void Start ()
 	{
 		offset = transform.position - player.transform.position;
+
+
+		float vertExtent = Camera.main.orthographicSize;
+		float horzExtent = vertExtent * Screen.width / Screen.height;
+
+//		xMin = (float)(horzExtent - mapX / 2.0);
+//		xMax = (float)(mapX / 2.0 - horzExtent);
+//		yMin = (float)(vertExtent - mapY / 2.0);
+//		yMax = (float)(mapY / 2.0 - vertExtent);
 	}
+
 	
 	void LateUpdate ()
 	{
 		transform.position = player.transform.position + offset;
+
+		Vector3 v2 = transform.position;
+		v2.x = Mathf.Clamp(v2.x, xMin, xMax);
+		v2.y = Mathf.Clamp(v2.y, yMin, yMax);
+		transform.position = v2 + offset;
 	}
 }
--- Battle_Balloons(2D)/Assets/Scripts/PlayerController.cs
+++ Battle_Balloons(2D)/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,11 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	private Rigidbody2D rb;
+	private float timeStamp = 0.0f;
 
 	public float speed;
+	public float jumpSpeed;
+	public float jumpCooldown;
 
 	void Start ()
 	{
@@ -21,5 +24,16 @@ public class PlayerController : MonoBehaviour {
 		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
 		
 		rb.AddForce (movement * speed);
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Jump ();
+		}
+	}
+	void Jump (){
+
+		if (timeStamp <= Time.time) {
+			rb.AddForce (Vector2.up * jumpSpeed);
+			timeStamp = Time.time + jumpCooldown;
+		}
 	}
 }
